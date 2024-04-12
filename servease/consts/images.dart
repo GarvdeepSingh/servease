264fb6d9e4617e519e6c3a5f6da4bd9834bc3ec6
Samardeep singh <124089261+samardeeps@users.noreply.github.com>
2024-04-12 12:27:45 +0530

    hdw

--- servease/consts/images.dart
+++ servease/consts/images.dart
@@ -1,2 +1,4 @@
 const logo = "assets/images/charity box.png";
 const p1 = "assets/images/man dancing with index finger pointing up";
+const p3 = "assets/images/man sitting and showing heart with his arms.png";
+const p4 = "assets/image/young women standing.png";
