2b2f07ee57b5fcf9cd43b81668dd76fb712f5a98
GarvdeepSingh <145035957+GarvdeepSingh@users.noreply.github.com>
2024-04-16 04:11:38 +0530

    i

--- servease/macos/Flutter/GeneratedPluginRegistrant.swift
+++ servease/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,18 +7,12 @@ import Foundation
 
 import firebase_auth
 import firebase_core
-<<<<<<< Updated upstream
 import geolocator_apple
-=======
 import google_sign_in_ios
->>>>>>> Stashed changes
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-<<<<<<< Updated upstream
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-=======
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
->>>>>>> Stashed changes
 }
