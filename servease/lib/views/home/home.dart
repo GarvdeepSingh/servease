2d7b79683a7551f56ac361a33df2c446de566752
GarvdeepSingh <145035957+GarvdeepSingh@users.noreply.github.com>
2024-04-17 21:37:54 +0530

    i

--- servease/lib/views/home/home.dart
+++ servease/lib/views/home/home.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:servease/consts/consts.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -10,49 +9,22 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
-  final User = FirebaseAuth.instance.currentUser;
 
-  CollectionReference _referenceInfoList =
-      FirebaseFirestore.instance.collection('contacts');
-  late Stream<QuerySnapshot> _streamInfoItems;
+  final User=FirebaseAuth.instance.currentUser;
 
   signout() async {
     await FirebaseAuth.instance.signOut();
   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    _streamInfoItems = _referenceInfoList.snapshots();
-  }
 
   @override
   Widget build(BuildContext context) {
-    // _referenceInfoList.get();
-    // _referenceInfoList.snapshots();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("home page"),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: _streamInfoItems,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-          if (snapshot.connectionState == ConnectionState.active) {
-            // ignore: unused_local_variable
-            QuerySnapshot querySnapshot = snapshot.data;
-          }
+      appBar: AppBar(title: Text("home page"),),
+      body: Center(child: Text('${User!.email}'),),
 
-          return Center(child: CircularProgressIndicator());
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => signout(),
-        child: Icon(Icons.login_rounded),
-      ),
+      floatingActionButton: FloatingActionButton(onPressed: ()=>signout(),
+      child: const Icon(Icons.login_rounded),),
     );
   }
 }
